66226dd7d3d4cd30bfde2c0b4a22b419394e39d6
nimz78 <nimazare1378@gmail.com>
2025-04-28 17:51:32 +0330

    fix[utils.py]: remove detectron2

--- utils.py
+++ utils.py
@@ -1,15 +1,36 @@
-from detectron2.data.datasets import register_coco_instances
+import os
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as T
 
-def register_cardd_dataset():
-    register_coco_instances(
-        "cardd_train",
-        {},
-        "./datasets/CarDD/annotations/train.json",
-        "./datasets/CarDD/train"
-    )
-    register_coco_instances(
-        "cardd_val",
-        {},
-        "./datasets/CarDD/annotations/val.json",
-        "./datasets/CarDD/val"
-    )
+
+class CarDDDataset(Dataset):
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(root)))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, self.imgs[idx])
+        img = Image.open(img_path).convert("RGB")
+
+        # فرض کنیم دیتاستت فقط عکس دارد (اگر ماسک داشتی اطلاع بده، تنظیمش میکنم)
+        target = {}
+        target["image_id"] = torch.tensor([idx])
+
+        if self.transforms:
+            img = self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
